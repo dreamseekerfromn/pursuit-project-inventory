0c6e193073e62f2a3304a07f32cde27836fd7be3
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-06-26 19:41:07 -0400

    v 0.1

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("hello world!")
+function submitEvent(){
+    
+    const form = document.querySelector(".new_item");
+
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        //let todo = e.target.todo.value;
+        const result = document.querySelector(".inventory_page");
+        const err = document.querySelector("#new_item_err");
+        const itemN = e.target.new_item_title.value;
+        const itemP = e.target.new_item_price.value;
+        const itemD = e.target.new_item_desc.value;
+        const itemS = e.target.new_item_stock.value;
+        
+
+        const p = document.querySelector(".item_list");
+
+        const table = document.createElement("table");
+        const item = `<tr><td>${itemN}</td><td>${itemP}</td><td>${itemS}</td><td>${itemD}</td></tr>`;
+
+        table.innerHTML += item;
+        p.append(table);
+        /*if(!todo){
+        err.textContent = "Error! To do cannot be empty!";
+        }
+        else{
+            err.textContent = "";
+            const li=document.createElement("li");
+            li.setAttribute("class", "todo_list");
+            li.textContent = todo;
+            li.addEventListener("click", event => {    
+                if(li.style.textDecoration == "line-through"){
+                    li.style.textDecoration = null;
+                }
+                else{
+                    li.style.textDecoration = "line-through";
+                }
+            })
+            result.appendChild(li);
+        }*/
+        
+        form.reset();
+    });
+}
+
+submitEvent();
